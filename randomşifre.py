8336af1733b422936e465f5ac26c184acc351eee
erkutr <erkuturkyilmaz@gmail.com>
2021-12-05 15:59:08 +0300

    Added GUI

--- randomşifre.py
+++ randomşifre.py
@@ -1,5 +1,9 @@
 import random
 import string
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
 
 def password(length,num=False,strength='zayıf'):
     """şifrenin uzunluğu, sayı istiyorsanız num,ve şifrenin gücü(zayıf,orta,güçlü)"""   
@@ -40,9 +44,44 @@ def password(length,num=False,strength='zayıf'):
     random.shuffle(pwd)
     return ''.join(pwd)
 
-print("Zayıf şifre",password(5,True))
 
-print("Güçlü şifre",password(10,True,'orta'))
 
-print("Çok güçlü şifre",password(15,True,'güçlü'))
+def generate_and_show():
+    pwd = password(length=length_var.get(), num=check_var.get(), strength=selected_diff.get())
+    messagebox.showinfo("Message","Generated password is {} ".format(pwd))
 
+
+tk = Tk()
+
+frm = ttk.Frame(tk, padding=200)
+
+
+
+
+frm.grid()
+ttk.Label(frm , text="Şifre zorluğunu seçiniz:").grid(column=0, row=2)
+
+selected_diff = StringVar(tk, value="zayıf")
+
+dropdown_for_strength = ttk.Combobox(frm, textvariable=selected_diff)
+dropdown_for_strength['values'] = ["zayıf","orta","güçlü"]
+
+dropdown_for_strength['state'] = 'readonly'
+dropdown_for_strength.grid(column=0, row=3)
+
+ttk.Label(frm, text="Şifre uzunluğunu seçiniz:").grid(column=0,row=0)
+length_var = IntVar(tk, value=8)
+
+length_of_pwd = ttk.Entry(frm,textvariable=length_var).grid(column=0,row=1)
+
+check_var = BooleanVar(tk)
+check_box = ttk.Checkbutton(frm, text="Number",variable=check_var)
+check_box.grid(column=0,row=4)
+
+
+button =  ttk.Button(frm , text="GENERATE", command=generate_and_show)
+button.grid(column=0, row=10)
+
+
+
+tk.mainloop()
